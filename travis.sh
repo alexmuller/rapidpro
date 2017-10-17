a9e5c9ce2f931cb6d38bc68cb698183c5553d714
Alex Muller <alex@mullr.net>
2017-10-17 11:54:57 +0100

    Run migrations test separately on Travis
    These don't affect the rest of the build so we can run them independently.

--- travis.sh
+++ travis.sh
@@ -5,8 +5,9 @@ set -x # Output commands run to see them in the Travis interface
 
 if [ "$TEST" == "lint" ]; then
     flake8
-elif [ "$TEST" == "build" ]; then
+elif [ "$TEST" == "migrations" ]; then
     python manage.py makemigrations --dry-run | grep 'No changes detected' || (echo 'There are changes which require migrations.' && exit 1)
+elif [ "$TEST" == "build" ]; then
     python manage.py collectstatic --noinput
     (! python manage.py compress --extension=".haml" --settings=temba.settings_travis | grep 'Error') || exit 1
     node_modules/karma/bin/karma start karma.conf.coffee --single-run --browsers PhantomJS
